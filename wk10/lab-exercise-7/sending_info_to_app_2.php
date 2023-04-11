d688113d8162ea6e34d50b139c6d677c088e9d09
cviennguyen <75710962+cviennguyen@users.noreply.github.com>
2023-04-11 14:38:47 -0400

    Delete wk10 directory

--- wk10/lab-exercise-7/sending_info_to_app_2.php
+++
@@ -1,8 +0,0 @@
-<form method="get">
-  <input name="q" placeholder="Enter Text">
-  <br />
-  <input type="submit" value="Go">
-</form>
-<?php
-echo isset($_GET['q']) ? htmlentities($_GET['q']) : "";
-?>
